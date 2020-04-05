f143f98eadf195246e112dc6661582aee31fd152
samteck <samarth.guptas.1995@gmail.com>
2020-04-05 11:39:33 +0530

    parse json string using string->char array using ArduinoJson

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 
@@ -8,7 +9,16 @@ SoftwareSerial esp8266(10, 11); // RX, TX
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-String jsonn ;
+String serial_input;
+const size_t capacity = JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 280;
+DynamicJsonDocument doc(capacity);
+const char* json;
+int cases;
+int death;
+int well;
+int data[3];
+
+
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -34,7 +44,40 @@ void loop() { // run over and over
 
 void getdata(){
   if (esp8266.available()) {
-     jsonn = esp8266.readString();
-     Serial.print(jsonn);
+     serial_input = esp8266.readString();
+     
+     int len = serial_input.length()+1;
+     char char_array[len];
+     serial_input.toCharArray(char_array,len);
+     
+     deserializeJson(doc, char_array);
+     cases = doc["cases"];
+     death = doc["deaths"];
+     well  = doc["recovered"];
+
+     show_cases(cases);
+     show_death(death);
+     show_well(well);
+
+    //  data[0] = cases;
+    //  data[1] = death;
+    //  data[2] = well;
+
+    //  Serial.println(cases);
+    //  Serial.println(death);
+    //  Serial.println(well);
+     
    }
+}
+
+void show_cases(int value){
+  Serial.println(value);
+}
+
+void show_death(int value){
+  Serial.println(value);
+}
+
+void show_well(int value){
+  Serial.println(value);
 }
